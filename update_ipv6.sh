80db8d08ec92ac67c195c9fc648fc51c87cddf9d
jbbodart <jbbodart@yahoo.com>
2018-02-12 11:44:19 +0100

    Check WAN IP for every subdomain

--- update_ipv6.sh
+++ update_ipv6.sh
@@ -5,8 +5,6 @@
 API="https://dns.api.gandi.net/api/v5/"
 IP_SERVICE="http://me.gandi.net"
 
-CURRENT_IPV6=$(dig AAAA ${DOMAIN} +short)
-
 if [[ -z "${FORCE_IPV6}" ]]; then
   WAN_IPV6=$(curl -s6 ${IP_SERVICE})
   if [[ -z "${WAN_IPV6}" ]]; then
@@ -17,19 +15,26 @@ else
   WAN_IPV6="${FORCE_IPV6}"
 fi
 
-if [ "${CURRENT_IPV6}" = "${WAN_IPV6}" ] ; then
-    echo "$(date "+[%Y-%m-%d %H:%M:%S]") [INFO] Current DNS AAAA record matches WAN IP (${CURRENT_IPV6}). Nothing to do."
-    exit 0
-fi
-
 for RECORD in ${RECORD_LIST//;/ } ; do
+  if [ "${RECORD} = "@" ] ;
+    SUBDOMAIN="${DOMAIN}"
+  else
+    SUBDOMAIN="${RECORD}.${DOMAIN}"
+  fi
+
+  CURRENT_IPV6=$(dig AAAA ${SUBDOMAIN} +short)
+  if [ "${CURRENT_IPV6}" = "${WAN_IPV6}" ] ; then
+    echo "$(date "+[%Y-%m-%d %H:%M:%S]") [INFO] Current DNS AAAA record for ${SUBDOMAIN} matches WAN IP (${CURRENT_IPV6}). Nothing to do."
+    continue
+  fi
+
   DATA='{"rrset_ttl": '${TTL}', "rrset_values": ["'${WAN_IPV6}'"]}'
   status=$(curl -s -w %{http_code} -o /dev/null -XPUT -d "${DATA}" \
     -H"X-Api-Key: ${APIKEY}" \
     -H"Content-Type: application/json" \
     "${API}/domains/${DOMAIN}/records/${RECORD}/AAAA")
   if [ "${status}" = '201' ] ; then
-    echo "$(date "+[%Y-%m-%d %H:%M:%S]") [OK] Updated ${RECORD} to ${WAN_IPV6}"
+    echo "$(date "+[%Y-%m-%d %H:%M:%S]") [OK] Updated ${SUBDOMAIN} to ${WAN_IPV6}"
   else
     echo "$(date "+[%Y-%m-%d %H:%M:%S]") [ERROR] API POST returned status ${status}"
   fi
