8c1b28e231a1c6ff0c89d5bd256ad9dab4587e4b
MrScaille <60929826+MrScaille@users.noreply.github.com>
2021-01-20 13:59:01 +0100

    Update update_ipv4.sh
    Modified old update_ipv4 to work with Nameshield REST API

--- update_ipv4.sh
+++ update_ipv4.sh
@@ -1,24 +1,18 @@
-#!/bin/sh
-#
-# Updates domain A zone record with WAN IP using Gandi's LiveDNS.
-
-# prevent shell to expand wildcard record
-set -f
-
-API="https://dns.api.gandi.net/api/v5/"
+#!/bin/bash
+API_URL="https://ote-api.nameshield.net/v1"
 IP_SERVICE="http://me.gandi.net"
 
-if [[ -z "${FORCE_IPV4}" ]]; then
-  WAN_IPV4=$(curl -s4 ${IP_SERVICE})
-  if [[ -z "${WAN_IPV4}" ]]; then
-    echo "$(date "+[%Y-%m-%d %H:%M:%S]") [ERROR] Something went wrong. Can not get your IPv4 from ${IP_SERVICE}"
+if [[ -z "${FORCE_IPV4}" ]]; then                                                                                
+  WAN_IPV4=$(curl -s4 ${IP_SERVICE})                                                                           
+  if [[ -z "${WAN_IPV4}" ]]; then                                                                                
+    echo "$(date "+[%Y-%m-%d %H:%M:%S]") [ERROR] Something went wrong. Can not get your IPv4 from ${IP_SERVICE}" 
     exit 1
   fi
 else
   WAN_IPV4="${FORCE_IPV4}"
 fi
 
-for RECORD in ${RECORD_LIST//;/ } ; do
+for RECORD in ${RECORD_LIST//;/ }; do
   if [ "${RECORD}" = "@" ] || [ "${RECORD}" = "*" ]; then
     SUBDOMAIN="${DOMAIN}"
   else
@@ -26,16 +20,19 @@ for RECORD in ${RECORD_LIST//;/ } ; do
   fi
 
   CURRENT_IPV4=$(dig A ${SUBDOMAIN} +short)
-  if [ "${CURRENT_IPV4}" = "${WAN_IPV4}" ] ; then
+
+  if [ "${CURRENT_IPV4}" = "${WAN_IPV4}" ]; then
     echo "$(date "+[%Y-%m-%d %H:%M:%S]") [INFO] Current DNS A record for ${RECORD} matches WAN IP (${CURRENT_IPV4}). Nothing to do."
     continue
   fi
 
-  DATA='{"rrset_ttl": '${TTL}', "rrset_values": ["'${WAN_IPV4}'"]}'
-  status=$(curl -s -w %{http_code} -o /dev/null -XPUT -d "${DATA}" \
-    -H"X-Api-Key: ${APIKEY}" \
+  DATA='{"name":"", "type": "A", "data": ["'${WAN_IPV4}'"] , "ttl": '${TTL}'}'
+
+  status=$(curl --location --request "${API_URL}/zones/${DOMAIN}/records" \
+    -H"Authorization: Bearer ${APIKEY}" \
     -H"Content-Type: application/json" \
-    "${API}/domains/${DOMAIN}/records/${RECORD}/A")
+    --data-raw "${DATA}"
+
   if [ "${status}" = '201' ] ; then
     echo "$(date "+[%Y-%m-%d %H:%M:%S]") [OK] Updated ${RECORD} to ${WAN_IPV4}"
   else
